f6b43e8e75299be6bebf6a204b72911bf5589249
fabioguelfi <fabioguelfiunix@gmail.com>
2018-04-28 16:00:17 -0300

    Criando o serviço do Jogo da velha

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,3 +1,4 @@
+import { JogoDaVelhaService } from './shared/jogo-da-velha.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
@@ -11,6 +12,9 @@ import { JogoDaVelhaComponent } from './jogo-da-velha.component';
   ],
   exports: [
     JogoDaVelhaComponent
+  ],
+  providers: [
+    JogoDaVelhaService
   ]
 })
 export class JogoDaVelhaModule { }
